28b160b8754a08a45fe57e077154362d876a2e26
Your Nameedwarrojas26 <edrojas843@misena.edu.co>
2022-09-28 13:03:24 -0500

    Proyecto_V2

--- react_js_leider_edwar/src/index.js
+++ react_js_leider_edwar/src/index.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-/*import Banner from './components/banner/Banner';*/
-import Navbar from './components/navbar/Navbar.js'
-import Foto from './components/foto/Foto.js'
-import Dash from './components/dashboard/Dashboard.js'
-
+import Banner from './components/banner/Banner';
 import './index.css';
 
 
@@ -12,9 +8,7 @@ import './index.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Navbar/>
-    <Foto/>
-    <Dash/>
+    <Banner/>
   </React.StrictMode>
 );
 
