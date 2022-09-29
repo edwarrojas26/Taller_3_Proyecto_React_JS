50dbd962ec3eb46183e48da7eb5ef8f49be84031
Your Nameedwarrojas26 <edrojas843@misena.edu.co>
2022-09-29 10:17:49 -0500

    Proyecto_V2 2.0

---
+++ react_js_leider_edwar/src/components/navbar/Navbar.js
@@ -0,0 +1,32 @@
+
+import logo from "./Hotelia horizontal blanco.svg"
+import "./navbar.css"
+function Navbar(){
+    return(
+        <nav>
+        <div className="logo">
+            <img src={logo} />
+            <div className="usuario">
+                <h3>Angie Vargas</h3>
+                <p>Angievargas2@gmail.com</p>
+            </div>
+        </div>
+        <div className="menu">
+            <div className="item">
+                <div><i className="fa-solid fa-bed"></i><a>Habitaciones</a></div>
+                <div><i className="fa-solid fa-bell-concierge"></i><a>Reservas</a></div>
+                <div><i className="fa-solid fa-bed"></i><a>Habitaciones</a></div>
+                <div><i className="fa-solid fa-bell-concierge"></i><a>Habitaciones</a></div>
+            </div>
+
+        </div>
+        <div className="exit">
+            <i className="fa-solid fa-right-from-bracket"></i><a>Cerrar sesión</a>
+        </div>
+    </nav>
+
+     
+    )
+}
+
+export default Navbar;
--- react_js_leider_edwar/src/index.js
+++ react_js_leider_edwar/src/index.js
@@ -1,15 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import Banner from './components/banner/Banner';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Navbar from "./components/navbar/Navbar.js";
+import Foto from "./components/foto/Foto.js";
+import Form from "./components/formulario/Formulario"
+// import Banner from './components/banner/Banner';
+import "./index.css";
 
-
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Banner/>
+    <Navbar/>
+    <section className="dashboard">
+      <div className="info">
+        <div className="bienvenido">
+          <h1>Bienvenido, Angie Vargas</h1>
+        </div>
+
+        
+        <div className="info">
+          <Foto/>
+          <Form/>
+        </div>
+      </div>
+    </section>
   </React.StrictMode>
 );
-
-
